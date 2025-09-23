542e5a89a28ff12dc47679ef4e8605a2b00eda90
Sapper <saniamykhailenko@gmail.com>
2025-09-23 14:34:40 +0300

    init repo

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,16 +1,66 @@
-import { Note } from "@/lib/api";
-import NoteItem from "../NoteItem/NoteItem";
+// import { Note } from "@/lib/api";
+// import NoteItem from "../NoteItem/NoteItem";
 
-type Props = {
+// type Props = {
+//   notes: Note[];
+// };
+
+// export default function NoteList({ notes }: Props) {
+//   return (
+//     <ul>
+//       {notes.map((note) => (
+//         <NoteItem key={note.id} item={note} />
+//       ))}
+//     </ul>
+//   );
+// }
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Note } from "../../types/note";
+import css from "./NoteList.module.css";
+import { deleteNote } from "../../services/noteService";
+import toast from "react-hot-toast";
+
+interface NoteListProps {
   notes: Note[];
-};
+}
 
-export default function NoteList({ notes }: Props) {
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const deleteNoteMutate = useMutation({
+    mutationFn: (id: string) => deleteNote(id),
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+    onError(error) {
+      toast.error(error.message);
+    },
+  });
+
+  function handleDeleteNote(id: string) {
+    deleteNoteMutate.mutate(id);
+  }
+
   return (
-    <ul>
-      {notes.map((note) => (
-        <NoteItem key={note.id} item={note} />
-      ))}
+    <ul className={css.list}>
+      {notes.map((note) => {
+        return (
+          <li className={css.listItem} key={note.id}>
+            <h2 className={css.title}>{note.title}</h2>
+            <p className={css.content}>{note.content}</p>
+            <div className={css.footer}>
+              <span className={css.tag}>{note.tag}</span>
+              <button
+                onClick={() => handleDeleteNote(note.id)}
+                className={css.button}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
